b926a59dad1090aff2c427828c7ee6924c1185a5
faithremo <faithmakumi@gmail.com>
2022-02-16 23:05:06 +0300

    add modifications to the form

--- src/app/user-profile.service.ts
+++ src/app/user-profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import 'rxjs/add/operator/map';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +9,22 @@ export class UserProfileService {
 
   private username:string;
   private access_token  = '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';
+  
 
   constructor(private ready:HttpClient) {
-    console.log("service is ready!");
+    // console.log("service is ready!");
     this.username = 'faithremo';
   }
 
   getUserInfo(){
     return this.ready.get("https://api.github.com/users/" + this.username + "?access_token=" + this.access_token)
-    .map(res => res.json());
+    
     
   }
 
   getProfileRepos(){
     return this.ready.get("https://api.github.com/users/" + this.username + "/repos?access_token=" + this.access_token)
-    .map(res => res.json());
+    
   }
 
   updateProfile(username: string){
